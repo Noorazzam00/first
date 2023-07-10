f27621004753af6acacc72c1fd082f7de8f865ca
NoorAzzam <noorazzam19@gmail.com>
2023-07-10 12:54:53 +0300

    Co-authored-by: Anas Zein <AnasZein@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    person : {
+      fullName : "Neymar Jr.",
+      bio : "lorem ipsum",
+      imageSrc: "https://b.fssta.com/uploads/application/soccer/headshots/713.png",
+      job : "football player"
+    },
+    show: false , 
+    interval: 0,
+  };
+
+
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      this.setState(prevState => ({
+        interval: prevState.interval + 1,
+      }));
+    }, 1000);
+  }
+
+  
+  componentWillUnmount() {
+      clearInterval(this.timer);
+  }
+
+
+  render() {
+    const { person, show , interval} = this.state
+    return (
+      <div>
+        <button onClick={()=>this.setState({show: !this.state.show, interval: 0})} >show</button>
+        {show && (
+          <h2>
+      <div>
+            <h1>{person.fullName}</h1>
+            <p>{person.bio}</p>
+            <img src={person.imageSrc} alt="Profile" />
+            <p>{person.profession}</p>
+          </div>          </h2>
+        )}
+        <p>Time interval since mount: {interval} seconds</p>
+
+      </div>
+    );
+  }
 }
 
 export default App;
